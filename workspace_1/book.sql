3ec95fee914f1fd8e5cd27aadf9d8a8493db60fc
oet <dhdmsxor1@naver.com>
2019-08-17 23:10:50 +0900

    8-17 게시판만들기

---
+++ workspace_1/book.sql
@@ -0,0 +1,69 @@
+create index idx_reply on tbl_reply (bno desc, rno asc);
+select rno, bno, reply, replyer, replydate, updatedate from(
+select /*+index(tbl_reply idx_reply)*/
+rownum rn, bno, rno, reply, replyer, replyDate, updatedate
+from tbl_reply where bno = 18 and rno >0 and rownum <=20)
+where rn >10;
+select rno, bno, reply, replyer, replyDate, updatedate from tbl_reply where bno = 18 order by rno asc;
+select * from tbl_reply order by rno desc;
+commit;
+select * from tbl_board where rownum < 10 order by bno desc;
+create SEQUENCE seq_reply;
+create table tbl_reply(
+    rno NUMBER(10,0),
+    bno number(10,0) not null,
+    reply varchar2(1000)not null,
+    replyer varchar2(50) not null,
+    replyDate date default sysdate,
+    updateDate date DEFAULT sysdate
+);
+alter table tbl_reply add constraint pk_reply primary key (rno);
+alter table tbl_reply add constraint fk_reply_board foreign key (bno) references tbl_board(bno);
+
+create index idx_reply on tbl_reply (bno desc, rno asc);
+select rno, bno, reply, replyer, replydate, updatedate from(
+select /*+index(tbl_reply idx_reply)*/
+rownum rn, bno, rno, reply, replyer, replyDate, updatedate
+from tbl_reply where bno = 18 and rno >0 and rownum <=20)
+where rn >10;
+select rno, bno, reply, replyer, replyDate, updatedate from tbl_reply where bno = 18 order by rno asc;
+select * from tbl_reply order by rno desc;
+commit;
+select * from tbl_board where rownum < 10 order by bno desc;
+create SEQUENCE seq_reply;
+create table tbl_reply(
+    rno NUMBER(10,0),
+    bno number(10,0) not null,
+    reply varchar2(1000)not null,
+    replyer varchar2(50) not null,
+    replyDate date default sysdate,
+    updateDate date DEFAULT sysdate
+);
+alter table tbl_reply add constraint pk_reply primary key (rno);
+alter table tbl_reply add constraint fk_reply_board foreign key (bno) references tbl_board(bno);
+-------------------------------------------
+SELECT DBMS_XDB.GETHTTPPORT() from dual;
+create SEQUENCE seq_board;
+create table tbl_board(
+    bno number(10,0),
+    title varchar2(200)not null,
+    content varchar2(2000) not null,
+    writer varchar2(50) not null,
+    regdate date default sysdate,
+    updatedate date DEFAULT sysdate
+);
+alter TABLE tbl_board add constraint pk_board
+primary key (bno);
+
+insert into tbl_board (bno, title, content, writer)
+values (seq_board.nextval, '테스트 제목', '테스트 내용', 'user00');
+
+insert into tbl_board (bno, title, content, writer)
+values (1, '스프링 테스트', '시작', 'oh e t');
+
+select * from TBL_BOARD;
+select bno, title, content, writer, regdate, updatedate from (select /*+index_desc(tbl_board pk_board) */
+  		rownum rn, bno, title, content, writer, regdate, updatedate from tbl_board where rownum <= 20)
+  		where rn > 10;
+        
+commit;
---
+++ workspace_1/ex02/src/main/java/org/zerock/mapper/BoardMapper.java
@@ -0,0 +1,26 @@
+package org.zerock.mapper;
+
+import java.util.List;
+
+
+import org.zerock.domain.BoardVO;
+import org.zerock.domain.Criteria;
+
+public interface BoardMapper {
+	//@Select("select * from tbl_board")
+	public List<BoardVO> getList();
+	
+	public void insert(BoardVO board);
+	
+	public void insertSelectKey(BoardVO board);
+	
+	public BoardVO read(Long bno);
+	
+	public int delete(Long bno);
+	
+	public int update(BoardVO board);
+	
+	public List<BoardVO> getListWithPaging(Criteria cri);
+	
+	public int getTotalCount(Criteria cri);
+}
